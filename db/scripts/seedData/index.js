e00247bb97e9e6ca16ca7dcf81a6fa8d3b6ad03d
Jeremy Law <jeremylaw2308@gmail.com>
2020-12-18 12:20:32 +0000

    adding seed data

---
+++ db/scripts/seedData/index.js
@@ -0,0 +1,32 @@
+const initialUser = {
+  id: 1,
+  name: 'Alice',
+  email: 'alice@alice.com',
+  password: 'password',
+  personality: 'INFP',
+  start_date: '20/9/2019',
+  points: 0,
+};
+
+const initialPost = {
+  id: 1,
+  user_id: 1,
+  post: 'I love the course',
+  multimedia: null,
+  date: '20/9/2019',
+  favorite: true,
+};
+
+const initialMood = {
+  id: 1,
+  user_id: 1,
+  mood: 5,
+  date: '20/9/2019',
+};
+
+const initialTrophy = {
+  id: 1,
+  user_id: 1,
+  trophy_name: 'Welcome Trophy',
+  date: '20/9/2019',
+};
