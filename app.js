e68877e8fb4e5eace194de065563dfdb862d6154
Anna-Marie Moss <ambrown1976@googlemail.com>
2021-01-04 00:28:00 +0000

    cloudinary installed and post model updated to send files to cloudinary, return the url and then post to the journal db table

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const cors = require("cors");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require("./routes/");
-var usersRouter = require("./routes/users/users");
-var postsRouter = require("./routes/posts/posts");
-var moodsRouter = require("./routes/moods/moods");
-var moodsAndPostsRouter = require("./routes/moodsAndPosts");
+var indexRouter = require('./routes/');
+var usersRouter = require('./routes/users/users');
+var postsRouter = require('./routes/posts/posts');
+var moodsRouter = require('./routes/moods/moods');
+var moodsAndPostsRouter = require('./routes/moodsAndPosts');
 
 var app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json({ limit: '50mb' })); // added limit code
+app.use(express.urlencoded({ limit: '50mb', extended: true })); // added limit code and changed extended to true
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use("/", indexRouter);
-app.use("/posts", postsRouter);
-app.use("/users", usersRouter);
-app.use("/moods", moodsRouter);
-app.use("/moodsandposts", moodsAndPostsRouter);
+app.use('/', indexRouter);
+app.use('/posts', postsRouter);
+app.use('/users', usersRouter);
+app.use('/moods', moodsRouter);
+app.use('/moodsandposts', moodsAndPostsRouter);
 
 module.exports = app;
